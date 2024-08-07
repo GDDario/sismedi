8af77587b61669ac34cc85688f419cba0a51bb30
GDDario <gd.dario.dev@gmail.com>
2024-08-06 21:23:29 -0300

    chore: upload login and users sketch and main colors definition

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                mainBrightBlue: 'rgb(var(--main-bright-blue))',
+                mainLightBlue: 'rgb(var(--main-light-blue))',
+                mainDarkBlue: 'rgb(var(--main-dark-blue))'
+            }
+        },
+    },
+    plugins: [],
 }
 
