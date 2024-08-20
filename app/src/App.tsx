1f39e7973bc4cb1b093147fd69591cb89269fbcb
GDDario <gd.dario.dev@gmail.com>
2024-08-19 23:00:55 -0300

    feat(app): global alert message component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,12 +3,14 @@ import {Provider} from "react-redux";
 import store from "./config/store.ts";
 import {RouterProvider} from "react-router-dom";
 import router from "./config/router.tsx";
+import Message from "./shared-components/Message/Message.tsx";
 
 function App() {
 
     return (
         <>
             <Provider store={store}>
+                <Message/>
                 <RouterProvider router={router}/>
             </Provider>
         </>
--- app/src/config/store.ts
+++ app/src/config/store.ts
@@ -1,10 +1,12 @@
 import {configureStore} from "@reduxjs/toolkit";
 import userReducer from '../features/authentication/store/userSlice';
+import messageReducer from '../store/messageSlice.ts';
 
 const store = configureStore(
     {
         reducer: {
             user: userReducer,
+            message: messageReducer
         }
     }
 );
