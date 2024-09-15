250e413ee6732695e71364c7536b8c82549e4777
GDDario <gd.dario.dev@gmail.com>
2024-09-14 22:31:38 -0300

    test(api): patients list tests

--- api/app/Models/Patient.php
+++ api/app/Models/Patient.php
@@ -3,10 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Auth\User as UserAlias;
 
-class Patient extends Model
+/**
+ * @extends
+ */
+class Patient extends UserAlias
 {
     use HasFactory;
 
--- api/database/seeders/PatientSeeder.php
+++ api/database/seeders/PatientSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Patient;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class PatientSeeder extends Seeder
