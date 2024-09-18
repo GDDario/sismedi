b94956aeda09680d0574c0d97ea18cfda7984674
GDDario <gd.dario.dev@gmail.com>
2024-09-18 19:50:38 -0300

    chore(api): add fix uuid to default patient user

--- api/database/seeders/PatientSeeder.php
+++ api/database/seeders/PatientSeeder.php
@@ -13,7 +13,7 @@ class PatientSeeder extends Seeder
     public function run(): void
     {
         // Accessible
-        Patient::factory()->withUser(['email' => 'user@patient.com', 'name' => 'Jhon Doe', 'cpf' => '44654545972'])->create(['cns' => '7093887818289', 'created_at' => '2024-09-02 03:50:29']);
+        Patient::factory()->withUser(['email' => 'user@patient.com', 'name' => 'Jhon Doe', 'cpf' => '44654545972'])->create(['uuid' => 'c9eff2e0-bd27-4c5f-930b-b12664801bcd', 'cns' => '7093887818289', 'created_at' => '2024-09-02 03:50:29']);
 
         Patient::factory()->withUser()->count(29)->create();
     }
