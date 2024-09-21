7da4002f8b182d3d6c5942ebc9bfa0af9ebeecfe
GDDario <gd.dario.dev@gmail.com>
2024-09-21 15:25:41 -0300

    chore(api): create states seeder

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
 //            UserSeeder::class,
+            StateSeeder::class,
             PatientSeeder::class
         ]);
     }
