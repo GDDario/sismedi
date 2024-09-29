7ae941e1b9d2d9db97587d1f6c1617e45015a97b
GDDario <gd.dario.dev@gmail.com>
2024-09-29 14:25:30 -0300

    feat(api): return cellphone in getPatientByUuid method

--- api/app/Services/PatientService.php
+++ api/app/Services/PatientService.php
@@ -3,9 +3,11 @@
 namespace App\Services;
 
 use App\Exceptions\NotFoundException;
+use App\Models\Cellphone;
 use App\Models\Patient;
 use App\Repositories\PatientRepository;
 use App\Util\PaginationUtil;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Response;
 
 class PatientService
@@ -38,7 +40,7 @@ class PatientService
         return new Response($pageData, Response::HTTP_OK);
     }
 
-    private function arrangePatientData(Patient $patientsData): array
+    private function arrangePatientData(Patient $patientData): array
     {
 
 //        dd($patientsData);
@@ -46,36 +48,45 @@ class PatientService
         return [
             'data' => [
                 'patient' => [
-                    'uuid' => $patientsData->uuid,
-                    'name' => $patientsData->user->name,
-                    'email' => $patientsData->user->email,
-                    'cpf' => $patientsData->user->cpf,
-                    'cns' => $patientsData->cns,
-                    'email_verified_at' => $patientsData->user->email_verified_at,
-                    'created_at' => $patientsData->created_at,
-                    'updated_at' => $patientsData->updated_at,
-                    'deleted_at' => $patientsData->deleted_at
+                    'uuid' => $patientData->uuid,
+                    'name' => $patientData->user->name,
+                    'email' => $patientData->user->email,
+                    'cpf' => $patientData->user->cpf,
+                    'cns' => $patientData->cns,
+                    'email_verified_at' => $patientData->user->email_verified_at,
+                    'created_at' => $patientData->created_at,
+                    'updated_at' => $patientData->updated_at,
+                    'deleted_at' => $patientData->deleted_at
                 ],
                 'address' => [
-                    'address_uuid' => $patientsData->address->uuid,
-                    'street_address' => $patientsData->address->street_address,
-                    'house_number' => $patientsData->address->house_number,
-                    'address_line_2' => $patientsData->address->address_line_2,
-                    'neighborhood' => $patientsData->address->neighborhood,
-                    'postal_code' => $patientsData->address->postal_code,
-                    'city_uuid' => $patientsData->address->city->uuid,
-                    'city_name' => $patientsData->address->city->name,
-                    'ibge_code' => $patientsData->address->city->ibge_code,
-                    'state_uuid' => $patientsData->address->city->state->uuid,
-                    'state_name' => $patientsData->address->city->state->name,
-                    'state_code' => $patientsData->address->city->state->code,
-                    'state_ibge_code' => $patientsData->address->city->state->ibge_code,
-                    'ddd' => $patientsData->address->city->state->ddd
+                    'address_uuid' => $patientData->address->uuid,
+                    'street_address' => $patientData->address->street_address,
+                    'house_number' => $patientData->address->house_number,
+                    'address_line_2' => $patientData->address->address_line_2,
+                    'neighborhood' => $patientData->address->neighborhood,
+                    'postal_code' => $patientData->address->postal_code,
+                    'city_uuid' => $patientData->address->city->uuid,
+                    'city_name' => $patientData->address->city->name,
+                    'ibge_code' => $patientData->address->city->ibge_code,
+                    'state_uuid' => $patientData->address->city->state->uuid,
+                    'state_name' => $patientData->address->city->state->name,
+                    'state_code' => $patientData->address->city->state->code,
+                    'state_ibge_code' => $patientData->address->city->state->ibge_code,
+                    'ddd' => $patientData->address->city->state->ddd
                 ],
-                'cellphones' => [
-
-                ]
+                'cellphones' => $this->arrangeCellphones($patientData->cellphones)
             ]
         ];
+    }
+
+    private function arrangeCellphones(Collection $cellphones): array
+    {
+        return $cellphones->map(function (Cellphone $cellphone) {
+            return [
+                'uuid' => $cellphone->uuid,
+                'number' => $cellphone->number,
+                'description' => $cellphone->description,
+            ];
+        })->toArray();
     }
 }
