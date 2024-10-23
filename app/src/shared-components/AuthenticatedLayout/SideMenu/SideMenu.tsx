4a0ac9775834bb97f703d62204753841cadbf07d
GDDario <gd.dario.dev@gmail.com>
2024-10-22 21:20:12 -0300

    feat(app): logout

--- app/src/shared-components/AuthenticatedLayout/SideMenu/SideMenu.tsx
+++ app/src/shared-components/AuthenticatedLayout/SideMenu/SideMenu.tsx
@@ -1,7 +1,17 @@
 import NavigationMenu from "./NavigationMenu.tsx";
 import logo from '../../../assets/images/logovert.png'
-import { BiSolidLogOut } from "react-icons/bi";
+import {BiSolidLogOut} from "react-icons/bi";
+import {AuthenticationService} from "../../../features/authentication/services/AuthenticationService.ts";
+import {useNavigate} from "react-router-dom";
+
 const SideMenu = () => {
+    const navigate = useNavigate();
+
+    const logout = async () => {
+        await AuthenticationService.logout();
+        navigate('login');
+    }
+
     return (
         <div className="relative h-screen min-w-[240px] w-[11vw]">
             <div
@@ -12,7 +22,10 @@ const SideMenu = () => {
 
                 <div className="banana flex-grow flex flex-col justify-end pb-4">
                     <button
-                        className="group w-[80%] h-6 px-2 py-4 border-black border flex justify-start items-center shadow-md rounded-tr-md rounded-br-md bg-mainWhite gap-2 transition-all hover:w-[90%]">
+                        className="group w-[80%] h-6 px-2 py-4 border-black border flex justify-start items-center shadow-md rounded-tr-md rounded-br-md bg-mainWhite
+                                   gap-2 transition-all hover:w-[90%]"
+                        onClick={() => logout()}
+                    >
                         <BiSolidLogOut className="transition-all text-[20px] group-hover:text-[24px]"/>
                         Sair
                     </button>
